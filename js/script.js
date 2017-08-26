738c5679d399ea9a381cb96613b8141a01360bdf
Howard Ting <cuberuru@gmail.com>
2017-08-26 14:40:43 -0700

    set up new schema

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* PAGE SELECTION */
 //HIDE ALL PAGE ELEMENTS & THEN DISPLAY HOME PAGE ONLY
 var pagewrapper = document.querySelectorAll(".main-wrapper .page");
 for (var item of pagewrapper) {
@@ -5,14 +6,27 @@ for (var item of pagewrapper) {
 }
 var currentpage = pagewrapper[0];
 currentpage.style.display = "block"
-
 //NAVIGATE BETWEEN DIFFERENT PAGE DISPLAYS ON CLICK OF EACH NAVIGATION LINK
 var navlinks = document.querySelectorAll("nav li a");
 for (var i = 0; i < navlinks.length; ++i) {
-    navlinks[i].addEventListener("click", pageswapper.bind(null, i), false);
+    navlinks[i].addEventListener("click", pageswapper.bind(null, i, pagewrapper), false);
 }
-function pageswapper(index) {
+function pageswapper(index, wrapper) {
     currentpage.style.display = "none";
-    currentpage = pagewrapper[index];
+    currentpage = wrapper[index];
     currentpage.style.display = "block";
 }
+
+/* LANGUAGE SELECTION:
+ON LOAD
+1. retrieve page_status from cache (english | chinese)
+2. if page_status isn't in cache, default english.
+3. else if (page_status == english): change color of english and set page to english page
+4. else: change color of chinese and set page to chinese page
+
+ON CLICK
+1. if language (e.g. ENGLISH) selected, save page_status (e.g. english)
+2. set page based on selected outcome
+*/
+
+var langcontainer = document.getElementById("lang-select");
